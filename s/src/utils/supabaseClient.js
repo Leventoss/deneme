d41eb3c0fdcfc91a017b207df7f5d2d9544931a1
Levent <130273298+Leventoss@users.noreply.github.com>
2025-04-04 07:42:00 +0300

    Delete s directory

--- s/src/utils/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-// src/utils/supabaseClient.js
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://your-project-url.supabase.co';
-const supabaseAnonKey = 'your-anon-key';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
